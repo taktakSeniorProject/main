a2d438964b039a1fa51b069920d0ce60c432e46a
iyed-benali <aveyrokepler86@gmail.com>
2023-02-15 10:13:48 +0100

    nav sideBar slider Search Cate filter search

--- server/routes/item.routes.js
+++ server/routes/item.routes.js
@@ -3,4 +3,5 @@ const itemController = require("../controllers/item.controller");
 
 router.get("/", itemController.selectAll);
 router.get("/getOne/:id",itemController.getOne)
+router.get('/HomePage',itemController.GetItemsToBuy)
 module.exports = router;
