7732a376bf6a19c20b041e072bff6cf829ae1d03
Mohamedazizmrabet <aziz48992@gmail.com>
2023-02-15 10:22:19 +0100

    only jwt log in sign up

---
+++ client/src/components/logIn/Login.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import { Button, Checkbox, Form, Input } from 'antd';
+import axios from 'axios';
+function Login() {
+  const verif=(email, password)=>{
+    axios.get(`http://localhost:3000/api/user/addUser`)
+  }
+      return (
+    <div><Form
+    name="basic"
+    labelCol={{
+      span: 8,
+    }}
+    wrapperCol={{
+      span: 16,
+    }}
+    style={{
+      maxWidth: 600,
+    }}
+    initialValues={{
+      remember: true,
+    }}
+    autoComplete="off"
+  >
+    <Form.Item
+      label="Username"
+      name="username"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your username!',
+        },
+      ]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item
+      label="Password"
+      name="password"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your password!',
+        },
+      ]}
+    >
+      <Input.Password />
+    </Form.Item>
+
+    <Form.Item
+      name="remember"
+      valuePropName="checked"
+      wrapperCol={{
+        offset: 8,
+        span: 16,
+      }}
+    >
+      <Checkbox>Remember me</Checkbox>
+    </Form.Item>
+
+    <Form.Item
+      wrapperCol={{
+        offset: 8,
+        span: 16,
+      }}
+    >
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form></div>
+  )
+}
+
+export default Login
